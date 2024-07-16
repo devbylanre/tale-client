ddc278cbe77ee834178112ca3b7117ec9527ff76
devbylanre <devbylanre@gmail.com>
2024-07-16 14:02:59 +0100

    page test

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const IndexPage = () => {
-  return <div>IndexPage</div>;
+  return (
+    <div style={{ maxWidth: '32rem', margin: '0 auto' }}>
+      <p className='text-24 text-center color-red-100 bg-red-50 weight-500 decorate-underline radius-sm'>
+        Capture magnificent shots
+      </p>
+    </div>
+  );
 };
 
 export default IndexPage;
