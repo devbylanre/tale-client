b496c043ea5c43a2eb6de691765ee9be405a5e0c
devbylanre <devbylanre@gmail.com>
2024-07-18 00:01:10 +0100

    added Box components

---
+++ src/components/Box/Box.tsx
@@ -0,0 +1,122 @@
+import React from 'react';
+import Polymorphic from '../../types/polymorphic';
+import boxVariants from './variants';
+import Variants from '../../types/variants';
+
+namespace Box {
+  export type Elements = 'div' | 'span';
+  export type Props<Element extends Elements = 'div'> =
+    Polymorphic.ComponentPropsWithRef<
+      Element,
+      Variants.Props<typeof boxVariants>
+    >;
+  export type Ref<Element extends Elements> = Polymorphic.ComponentRef<Element>;
+}
+
+const Box = React.forwardRef(
+  <Element extends Box.Elements>(
+    props: Box.Props<Element>,
+    ref: Box.Ref<Element>
+  ) => {
+    const {
+      as,
+      m,
+      mx,
+      my,
+      ms,
+      me,
+      mt,
+      mb,
+      p,
+      px,
+      py,
+      ps,
+      pe,
+      pt,
+      pb,
+      spaceX,
+      spaceY,
+      border,
+      borderTop,
+      borderLeft,
+      borderRight,
+      borderBottom,
+      borderColor,
+      backgroundColor,
+      width,
+      minWidth,
+      maxWidth,
+      height,
+      minHeight,
+      maxHeight,
+      flex,
+      flexBasis,
+      alignSelf,
+      placeSelf,
+      justifySelf,
+      scale,
+      rotate,
+      translateX,
+      translateY,
+      transitionProperty,
+      transitionDelay,
+      transitionTimingFunction,
+      transitionDuration,
+      ...rest
+    } = props;
+    const Component = (as || 'div') as React.ElementType;
+
+    return (
+      <Component
+        ref={ref}
+        className={boxVariants({
+          m,
+          mx,
+          my,
+          ms,
+          me,
+          mt,
+          mb,
+          p,
+          px,
+          py,
+          ps,
+          pe,
+          pt,
+          pb,
+          spaceX,
+          spaceY,
+          border,
+          borderTop,
+          borderLeft,
+          borderRight,
+          borderBottom,
+          borderColor,
+          backgroundColor,
+          width,
+          minWidth,
+          maxWidth,
+          height,
+          minHeight,
+          maxHeight,
+          flex,
+          flexBasis,
+          alignSelf,
+          placeSelf,
+          justifySelf,
+          scale,
+          rotate,
+          translateX,
+          translateY,
+          transitionProperty,
+          transitionDelay,
+          transitionTimingFunction,
+          transitionDuration,
+        })}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default Box;
---
+++ src/components/Box/variants.ts
@@ -0,0 +1,66 @@
+import border from '../../props/border';
+import colors from '../../props/colors';
+import flex from '../../props/flex';
+import grid from '../../props/grid';
+import justification from '../../props/justification';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import transform from '../../props/transform';
+import transition from '../../props/transition';
+import cv from '../../utils/cv';
+
+const boxVariants = cv({
+  variants: {
+    m: spacing.margin,
+    mt: spacing.marginTop,
+    mb: spacing.marginBottom,
+    me: spacing.marginEnd,
+    ms: spacing.marginStart,
+    mx: spacing.marginX,
+    my: spacing.marginY,
+    p: spacing.padding,
+    pt: spacing.paddingTop,
+    pb: spacing.paddingBottom,
+    pe: spacing.paddingEnd,
+    ps: spacing.paddingStart,
+    px: spacing.paddingX,
+    py: spacing.paddingY,
+    spaceX: spacing.spaceX,
+    spaceY: spacing.spaceY,
+    border: border.width,
+    borderTop: border.top,
+    borderBottom: border.bottom,
+    borderLeft: border.left,
+    borderRight: border.right,
+    borderRadius: border.radius,
+    borderColor: colors.border,
+    backgroundColor: colors.background,
+    width: sizing.width,
+    minWidth: sizing.minWidth,
+    maxWidth: sizing.maxWidth,
+    height: sizing.height,
+    minHeight: sizing.minHeight,
+    maxHeight: sizing.maxHeight,
+    flexBasis: flex.basis,
+    flex: flex.flex,
+    gridColumnSpan: grid.columnSpan,
+    gridColumnStart: grid.columnStart,
+    gridColumnEnd: grid.columnEnd,
+    gridRowSpan: grid.rowSpan,
+    gridRowStart: grid.rowStart,
+    gridRowEnd: grid.rowEnd,
+    alignSelf: justification.alignSelf,
+    justifySelf: justification.justifySelf,
+    placeSelf: justification.placeSelf,
+    scale: transform.scale,
+    rotate: transform.rotate,
+    translateX: transform.translateX,
+    translateY: transform.translateY,
+    transitionProperty: transition.property,
+    transitionDuration: transition.duration,
+    transitionDelay: transition.delay,
+    transitionTimingFunction: transition.timing,
+  },
+});
+
+export default boxVariants;
