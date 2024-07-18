8855563a4dfd23aa34a91d6e95698ef7c4adbd7f
devbylanre <devbylanre@gmail.com>
2024-07-18 12:59:42 +0100

    user registartion page

---
+++ src/pages/RegisterPage.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import RegisterFeature from '../features/register/Feature';
+
+const RegisterPage = () => {
+  return <RegisterFeature />;
+};
+
+export default RegisterPage;
