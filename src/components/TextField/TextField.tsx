3fbce41fd02caea7c2fb618a217a9b495a0be8ef
devbylanre <devbylanre@gmail.com>
2024-07-18 13:00:50 +0100

    TextField component

---
+++ src/components/TextField/TextField.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import Polymorphic from '../../types/polymorphic';
+import useField from '../../hooks/useField';
+import Variants from '../../types/variants';
+import textFieldVariants from './variants';
+
+namespace TextField {
+  export type Elements = 'input';
+  export type Props<E extends Elements> = Polymorphic.ComponentProps<
+    E,
+    Variants.Props<typeof textFieldVariants>
+  >;
+  export type Ref<E extends Elements> = Polymorphic.ComponentRef<E>;
+}
+
+const TextField = React.forwardRef(
+  <E extends TextField.Elements>(
+    props: TextField.Props<E>,
+    ref: TextField.Ref<E>
+  ) => {
+    const {
+      as,
+      onChange,
+      onFocus,
+      onBlur,
+      size,
+      weight,
+      width,
+      align,
+      transform,
+      color,
+      backgroundColor,
+      height,
+      style,
+      ...rest
+    } = props;
+    const Component = (as || 'input') as React.ElementType;
+    const { helper } = useField();
+
+    const handleChange = (e: any) => {
+      helper.setValue(e.target.value);
+      onChange && onChange(e);
+    };
+
+    const handleFocus = (e: any) => {
+      helper.setTouched(true);
+      onFocus && onFocus(e);
+    };
+    const handleBlur = (e: any) => {
+      helper.setTouched(false);
+      onBlur && onBlur(e);
+    };
+
+    const defaultStyle: React.CSSProperties = {
+      outline: 'none',
+    };
+
+    return (
+      <Component
+        ref={ref}
+        onBlur={handleBlur}
+        onFocus={handleFocus}
+        onChange={handleChange}
+        style={{ ...defaultStyle, ...style }}
+        className={textFieldVariants({
+          width,
+          height,
+          align,
+          transform,
+          color,
+          size,
+          backgroundColor,
+        })}
+        {...rest}
+      />
+    );
+  }
+);
+
+export default TextField;
---
+++ src/components/TextField/variants.ts
@@ -0,0 +1,29 @@
+import colors from '../../props/colors';
+import sizing from '../../props/sizing';
+import spacing from '../../props/spacing';
+import typography from '../../props/typography';
+import cv from '../../utils/cv';
+
+const textFieldVariants = cv({
+  variants: {
+    px: spacing.paddingX,
+    width: sizing.width,
+    height: sizing.height,
+    size: typography.size,
+    weight: typography.weight,
+    align: typography.align,
+    transform: typography.transform,
+    color: colors.text,
+    backgroundColor: colors.background,
+  },
+
+  defaultVariants: {
+    px: 'sm',
+    width: 'full',
+    height: 'full',
+    color: 'gray-10',
+    backgroundColor: 'inherit',
+  },
+});
+
+export default textFieldVariants;
