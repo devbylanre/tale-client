5098f4d5a0892b1f5e075d5b7326912d75c20eb3
devbylanre <devbylanre@gmail.com>
2024-07-19 10:54:44 +0100

    imported Header and Footer

--- src/features/register/Feature.tsx
+++ src/features/register/Feature.tsx
@@ -2,12 +2,20 @@ import React from 'react';
 import Section from '../../components/Section/Section';
 import Container from '../../components/Container/Container';
 import RegisterForm from './components/RegisterForm';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const RegisterFeature = () => {
   return (
     <Section>
-      <Container py={'3xl'}>
+      <Container
+        px={'md'}
+        py={'3xl'}
+        spaceY={'3xl'}
+      >
+        <Header />
         <RegisterForm />
+        <Footer />
       </Container>
     </Section>
   );
