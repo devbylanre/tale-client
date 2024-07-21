5077d8f2265bc7995c6085525ab44b632397ae4a
devbylanre <devbylanre@gmail.com>
2024-07-21 09:57:28 +0100

    updated function

--- src/utils/cv.ts
+++ src/utils/cv.ts
@@ -1,27 +1,30 @@
 import Variants from '../types/variants';
 
 const sortVariants = (props: {
-  variant: Variants.Config;
   values: any;
+  pseudo?: string;
+  variant: Variants.Config;
   classes: string[];
 }) => {
-  const { variant, values, classes } = props;
+  const { variant, pseudo, values, classes } = props;
 
   let result = '';
 
   if (values !== undefined) {
     if (variant.responsive === true) {
       if (typeof values !== 'object') {
-        result = `${variant.class}-${values}`;
+        result = `${pseudo ? pseudo + ':' : ''}${variant.class}-${values}`;
         classes.push(result);
       } else {
         Object.entries(values).forEach(([key, value]) => {
-          result = `${key}:${variant.class}-${value}`;
+          result = `${pseudo ? pseudo + ':' : ''}:${key}:${
+            variant.class
+          }-${value}`;
           classes.push(result);
         });
       }
     } else {
-      result = `${variant.class}-${values}`;
+      result = `${pseudo ? pseudo + ':' : ''}${variant.class}-${values}`;
       classes.push(result);
     }
   }
@@ -31,21 +34,48 @@ const cv = <Map extends Variants.Map>(args: {
   variants: Map;
   defaultVariants?: Variants.Values<Map>;
 }) => {
-  return (props: Variants.Values<Map>) => {
+  return (
+    props: Variants.Values<Map> & {
+      className?: string;
+      pseudos?: Variants.PseudosValues<Map>;
+    }
+  ) => {
     const { variants, defaultVariants } = args;
-    const { ...objects } = props;
+    const { className, pseudos, ...maps } = props;
     const classes: string[] = [];
 
-    Object.entries(objects).forEach(([key]) => {
-      const variant = variants[key];
-      const values = objects[key];
-      sortVariants({ variant, values, classes });
-    });
-
-    if (defaultVariants !== undefined) {
+    if (defaultVariants) {
       Object.entries(defaultVariants).forEach(([key, values]) => {
         const variant = variants[key];
-        sortVariants({ variant, values, classes });
+        return sortVariants({ variant, values, classes });
+      });
+    }
+
+    Object.entries(maps).forEach(([key, values]) => {
+      const variant = variants[key];
+
+      if (values) {
+        variant.values.forEach((value) => {
+          const classToRemove = `${variant.class}-${value}`;
+          const index = classes.indexOf(classToRemove);
+
+          if (index !== -1) {
+            classes.splice(index, 1);
+            return;
+          }
+        });
+        sortVariants({ variant: variant, values: values, classes: classes });
+      }
+    });
+
+    if (pseudos) {
+      Object.entries(pseudos).forEach(([pseudo, maps]) => {
+        Object.entries(maps).forEach(([key]) => {
+          const variant = variants[key];
+          const values = maps[key];
+
+          sortVariants({ pseudo: pseudo, variant, values: values, classes });
+        });
       });
     }
 
