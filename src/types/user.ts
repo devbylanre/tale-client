de3434d4ebc60ca9ec0d6f702278fda18ef9182e
devbylanre <devbylanre@gmail.com>
2024-07-22 22:10:03 +0100

    user type

---
+++ src/types/user.ts
@@ -0,0 +1,12 @@
+namespace User {
+  export type Props = {
+    _id: string;
+    firstName: string;
+    lastName: string;
+    role: string;
+    email: string;
+    status: string;
+  };
+}
+
+export default User;
