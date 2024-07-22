50b281b0814fba6759136f5810b5d4a69ee127d2
devbylanre <devbylanre@gmail.com>
2024-07-22 22:20:40 +0100

    bug fix

--- src/features/register/components/RegisterForm.tsx
+++ src/features/register/components/RegisterForm.tsx
@@ -6,7 +6,7 @@ import Box from '../../../components/Box/Box';
 import { TbEye, TbEyeClosed } from 'react-icons/tb';
 import Button from '../../../components/Button/Button';
 import { useMutation } from '@apollo/client';
-import auth from '../../../apis/auth';
+import auth from '../../../apis/authApi';
 import * as Yup from 'yup';
 import Alert from '../../../components/Alert/Alert';
 import Text from '../../../components/Text/Text';
@@ -25,7 +25,7 @@ const validationSchema = Yup.object().shape({
     .matches(/[a-z]/, 'Password must contain at least one lowercase letter')
     .matches(/[0-9]/, 'Password must contain at least one number')
     .matches(
-      /[@$!%*?&#]/,
+      /[@$!%*?&#.]/,
       'Password must contain at least one special character'
     ),
 });
