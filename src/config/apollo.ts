dc3a145e4a4c9cdf41ddb9cf9ac54d9ccde90838
devbylanre <devbylanre@gmail.com>
2024-07-22 22:23:24 +0100

    context setup

--- src/config/apollo.ts
+++ src/config/apollo.ts
@@ -1,7 +1,24 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:4000/graphql/',
+});
+
+const authContext = setContext((request) => {
+  let TOKEN = JSON.parse(localStorage.getItem('accessToken') || '');
+
+  console.log(request.context);
+
+  return {
+    headers: {
+      authorization: TOKEN ? `Bearer ${TOKEN}` : '',
+    },
+  };
+});
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql/',
+  link: authContext.concat(httpLink),
   credentials: 'include',
   cache: new InMemoryCache(),
 });
