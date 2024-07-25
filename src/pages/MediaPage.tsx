a2f3acbaa4e56482dd8ad31e93f19a677c57a71b
devbylanre <devbylanre@gmail.com>
2024-07-25 21:21:50 +0100

    media page

---
+++ src/pages/MediaPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import MediasFeature from '../features/medias/Feature';
+
+const MediaPage = () => {
+  return (
+    <React.Fragment>
+      <MediasFeature />
+    </React.Fragment>
+  );
+};
+
+export default MediaPage;
