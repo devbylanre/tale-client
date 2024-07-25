9c924f6cd216c8dab2d9eb6e9782e7b1ecd4e970
devbylanre <devbylanre@gmail.com>
2024-07-25 21:22:15 +0100

    category page

---
+++ src/pages/CategoryPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CategoryFeature from '../features/categories/Feature';
+
+const CategoryPage = () => {
+  return (
+    <React.Fragment>
+      <CategoryFeature />
+    </React.Fragment>
+  );
+};
+
+export default CategoryPage;
