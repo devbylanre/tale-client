3211e2949d256f2359bc26990dacab17e9421a52
devbylanre <devbylanre@gmail.com>
2024-07-31 08:11:03 +0100

    feat(media detail): rendering media data

--- src/features/mediaDetail/components/Base.tsx
+++ src/features/mediaDetail/components/Base.tsx
@@ -1,28 +1,63 @@
 import React from 'react';
 import Text from '../../../components/Text/Text';
 import Box from '../../../components/Box/Box';
+import useMedia from '../../../hooks/useMedia';
+import Button from '../../../components/Button/Button';
+import Icon from '../../../components/Icon/Icon';
+import { TbX } from 'react-icons/tb';
+import Flex from '../../../components/Flex/Flex';
+import { useSearchParams } from 'react-router-dom';
 
 const Base = () => {
+  const [_, setSearchParams] = useSearchParams();
+  const { media, setMedia } = useMedia();
+
+  const handleClear = () => {
+    setSearchParams('');
+    setMedia({ type: 'CLEAR_SINGLE', payload: null });
+  };
+
   return (
-    <Box
-      mx={'auto'}
-      spaceY={'sm'}
-      width={'full'}
-    >
-      <img
-        alt={'dummy'}
-        className={'w-full radius-xl'}
-        src={'/assets/images/dummy.jpg'}
-        style={{ objectFit: 'cover', aspectRatio: '1/1' }}
-      />
-      <Text
-        as={'h3'}
-        weight={400}
-        align={'center'}
+    <React.Fragment>
+      <Flex justifyContent={'center'}>
+        <Button
+          px={'md'}
+          gap={'xs'}
+          width={'fit'}
+          height={'24'}
+          borderRadius={'max'}
+          onClick={handleClear}
+          backgroundColor={'gray-70'}
+          pseudos={{ hover: { backgroundColor: 'gray-80' } }}
+        >
+          <Icon
+            size={20}
+            iconType={TbX}
+          />
+        </Button>
+      </Flex>
+
+      <Box
+        mt={'lg'}
+        mx={'auto'}
+        spaceY={'sm'}
+        width={'full'}
       >
-        Nature shot - Alabama United States
-      </Text>
-    </Box>
+        <img
+          className={'w-full radius-xl'}
+          alt={media ? media.alt : ''}
+          src={media ? media.uri : ''}
+          style={{ objectFit: 'cover', aspectRatio: '1/1' }}
+        />
+        <Text
+          as={'h3'}
+          weight={400}
+          align={'center'}
+        >
+          {media ? media.name : ''}
+        </Text>
+      </Box>
+    </React.Fragment>
   );
 };
 
