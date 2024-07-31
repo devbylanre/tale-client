68830f6d0061858bf4252b1633c92e5739059970
devbylanre <devbylanre@gmail.com>
2024-07-31 08:09:34 +0100

    feat(media detail): fetching media from db

--- src/features/mediaDetail/Feature.tsx
+++ src/features/mediaDetail/Feature.tsx
@@ -1,16 +1,48 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Base from './components/Base';
 import Information from './components/Information';
 import Actions from './components/Actions';
+import { useLazyQuery } from '@apollo/client';
+import { GET_SINGLE_MEDIA } from '../../apis/mediaApi';
+import { useLocation } from 'react-router-dom';
+import Media from '../../types/media';
+import useMedia from '../../hooks/useMedia';
 
 const MediaDetailFeature = () => {
-  return (
-    <React.Fragment>
-      <Base />
-      <Information />
-      <Actions />
-    </React.Fragment>
-  );
+  const { setMedia } = useMedia();
+  const location = useLocation();
+  const ID = location.search.split('=')[1];
+  const [getMedia, { loading, error }] = useLazyQuery<{
+    media: Media.Type;
+  }>(GET_SINGLE_MEDIA, {
+    variables: {
+      id: ID,
+    },
+    onCompleted: ({ media }) => {
+      setMedia({ type: 'READ_SINGLE', payload: media });
+    },
+  });
+
+  useEffect(() => {
+    if (ID !== undefined) {
+      getMedia();
+    }
+  }, [ID, getMedia]);
+
+  switch (true) {
+    case loading:
+      return null;
+    case error !== undefined:
+      return null;
+    default:
+      return (
+        <React.Fragment>
+          <Base />
+          <Information />
+          <Actions />
+        </React.Fragment>
+      );
+  }
 };
 
 export default MediaDetailFeature;
