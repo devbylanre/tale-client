294ced103a2503ce4bc27be0fb5bf10c631e8115
devbylanre <devbylanre@gmail.com>
2024-08-05 14:20:30 +0100

    component(Tabs): added provider ad compound components

---
+++ src/components/Tabs/Tabs.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import TabsProvider from '../../contexts/TabsContext';
+import Flex from '../Flex/Flex';
+import Button from '../Button/Button';
+import useTabs from '../../hooks/useTabs';
+
+type TabsProps = React.ComponentProps<typeof TabsProvider>;
+
+const Tabs = (props: TabsProps) => {
+  return <TabsProvider {...props} />;
+};
+
+const Triggers = React.forwardRef(
+  <E extends Flex.Elements>(props: Flex.Props<E>, ref: Flex.Ref<E>) => {
+    const {
+      p = '2xs',
+      gap = '2xs',
+      borderRadius = 'lg',
+      backgroundColor = 'gray-90',
+      ...rest
+    } = props;
+
+    return (
+      <Flex
+        ref={ref}
+        p={p}
+        gap={gap}
+        borderRadius={borderRadius}
+        backgroundColor={backgroundColor}
+        {...rest}
+      />
+    );
+  }
+);
+
+const Trigger = React.forwardRef(
+  (props: Button.Props & { value: string }, ref: Button.Ref) => {
+    const { activeTab, setActiveTab } = useTabs();
+    const isActive = activeTab === props.value;
+    const {
+      style,
+      onClick,
+      size = 13,
+      weight = 600,
+      height = '32',
+      width = 'full',
+      color = 'gray-10',
+      borderRadius = 'md',
+      backgroundColor = isActive ? 'white' : 'inherit',
+      pseudos = {
+        hover: { backgroundColor: isActive ? 'gray-100' : 'gray-95' },
+      },
+      ...rest
+    } = props;
+
+    const defaultStyle: React.CSSProperties = {
+      boxShadow: isActive ? '1px 2px 5px 0px rgba(0, 0, 0, 0.1)' : undefined,
+    };
+
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      setActiveTab(props.value);
+      onClick && onClick(e);
+    };
+
+    return (
+      <Button
+        ref={ref}
+        size={size}
+        width={width}
+        color={color}
+        weight={weight}
+        height={height}
+        borderRadius={borderRadius}
+        backgroundColor={backgroundColor}
+        pseudos={pseudos}
+        style={{ ...defaultStyle, ...style }}
+        onClick={handleClick}
+        {...rest}
+      />
+    );
+  }
+);
+
+const Body = React.forwardRef(
+  <E extends Flex.Elements>(
+    props: Flex.Props<E> & { value: string },
+    ref: Flex.Ref<E>
+  ) => {
+    const { activeTab } = useTabs();
+    const { value, flexDirection = 'column', ...rest } = props;
+
+    if (value !== activeTab) return null;
+
+    return (
+      <Flex
+        ref={ref}
+        flexDirection={flexDirection}
+        {...rest}
+      />
+    );
+  }
+);
+
+Tabs.Triggers = Triggers;
+Tabs.Trigger = Trigger;
+Tabs.Body = Body;
+export default Tabs;
