886c20efcfd0916af29d6dd779662d61ec373f7e
devbylanre <devbylanre@gmail.com>
2024-08-05 14:22:52 +0100

    feat(media-panel): media panel feature

---
+++ src/features/mediaPanel/Feature.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header';
+
+const MediaPanelFeature = () => {
+  return (
+    <React.Fragment>
+      <Header />
+    </React.Fragment>
+  );
+};
+
+export default MediaPanelFeature;
---
+++ src/features/mediaPanel/components/Header.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Tabs from '../../../components/Tabs/Tabs';
+import Flex from '../../../components/Flex/Flex';
+import Button from '../../../components/Button/Button';
+import Icon from '../../../components/Icon/Icon';
+import { IoResize } from 'react-icons/io5';
+import { useSearchParams } from 'react-router-dom';
+import useMedia from '../../../hooks/useMedia';
+import MediaDetailFeature from '../../mediaDetail/Feature';
+
+const Header = () => {
+  const { setMedia } = useMedia();
+  const [, setSearchParams] = useSearchParams();
+
+  const handleHide = () => {
+    setSearchParams('');
+    setMedia({ type: 'CLEAR_SINGLE', payload: null });
+  };
+
+  return (
+    <Tabs defaultTab={'about'}>
+      <Flex
+        px={'lg'}
+        height={'48'}
+        borderBottom={'1'}
+        alignItems={'center'}
+        borderColor={'gray-95'}
+        justifyContent={'between'}
+      >
+        <Tabs.Triggers>
+          <Tabs.Trigger value={'about'}>About</Tabs.Trigger>
+          <Tabs.Trigger value={'update'}>Update</Tabs.Trigger>
+          <Tabs.Trigger value={'delete'}>Delete</Tabs.Trigger>
+        </Tabs.Triggers>
+
+        <Button
+          px={'none'}
+          width={'32'}
+          height={'32'}
+          color={'gray-40'}
+          borderRadius={'max'}
+          onClick={handleHide}
+          backgroundColor={'inherit'}
+          pseudos={{ hover: { backgroundColor: 'gray-90' } }}
+        >
+          <Icon
+            size={20}
+            iconType={IoResize}
+          />
+        </Button>
+      </Flex>
+
+      <Tabs.Body
+        p={'lg'}
+        flex={'full'}
+        value={'about'}
+        backgroundColor={'gray-100'}
+      >
+        <MediaDetailFeature />
+      </Tabs.Body>
+    </Tabs>
+  );
+};
+
+export default Header;
