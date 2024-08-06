c6f659a65eb7bb21c71bd4ce775a920b02d186ed
devbylanre <devbylanre@gmail.com>
2024-08-06 23:36:33 +0100

    new providers component & page components

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,16 @@ import IndexPage from './pages/IndexPage';
 import RegisterPage from './pages/RegisterPage';
 import AuthLayout from './layouts/auth/Layout';
 import LoginPage from './pages/LoginPage';
+import AppLayout from './layouts/app/Layout';
+import UserProvider from './contexts/UserContext';
+import CategoryPage from './pages/CategoryPage';
+import MediaPage from './pages/MediaPage';
+import MediaProvider from './contexts/MediaContext';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from './configs/apolloConfig';
+import queryClient from './configs/queryConfig';
+import AuthProvider from './contexts/AuthContext';
 
 const router = createBrowserRouter([
   {
@@ -18,12 +28,32 @@ const router = createBrowserRouter([
           { path: 'user', element: <LoginPage /> },
         ],
       },
+      {
+        path: 'app',
+        element: <AppLayout />,
+        children: [
+          { path: 'media', element: <MediaPage /> },
+          { path: 'categories', element: <CategoryPage /> },
+        ],
+      },
     ],
   },
 ]);
 
 const Router = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ApolloProvider client={apolloClient}>
+        <AuthProvider>
+          <UserProvider>
+            <MediaProvider>
+              <RouterProvider router={router} />
+            </MediaProvider>
+          </UserProvider>
+        </AuthProvider>
+      </ApolloProvider>
+    </QueryClientProvider>
+  );
 };
 
 export default Router;
