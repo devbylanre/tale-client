775e9e8ea26223295f3f5de8470c55860c9ad24e
devbylanre <devbylanre@gmail.com>
2024-08-06 23:35:25 +0100

    removed ApolloProvider component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React from 'react';
 import Router from './Router';
-import { ApolloProvider } from '@apollo/client';
-import client from './config/apollo';
 
 function App() {
-  return (
-    <ApolloProvider client={client}>
-      <Router />
-    </ApolloProvider>
-  );
+  return <Router />;
 }
 
 export default App;
