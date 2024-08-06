efdf522633042f48642625ed6950f2356fa4f67f
devbylanre <devbylanre@gmail.com>
2024-08-06 23:16:09 +0100

    feat(delete media): render related components

---
+++ src/features/mediaDelete/Feature.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Banner from './components/Banner';
+import Header from './components/Header';
+import Detail from './components/Detail';
+import FormikHandler from './components/FormikHandler';
+
+const MediaDeleteFeature = () => {
+  return (
+    <React.Fragment>
+      <Banner />
+      <Header />
+      <Detail />
+      <FormikHandler />
+    </React.Fragment>
+  );
+};
+
+export default MediaDeleteFeature;
