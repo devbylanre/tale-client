3c3a507786025524419447f3f36a27f16dd8502c
devbylanre <devbylanre@gmail.com>
2024-08-06 23:33:54 +0100

    feat(media update): render feature components

---
+++ src/features/mediaUpdate/Feature.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FormikHandler from './components/FormikHandler';
+import Header from './components/Header';
+
+const MediaUpdateFeature = () => {
+  return (
+    <React.Fragment>
+      <Header />
+      <FormikHandler />
+    </React.Fragment>
+  );
+};
+
+export default MediaUpdateFeature;
