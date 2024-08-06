1fceaad1804fdaa2d3b1f4e8013cf7fe26a74b7c
devbylanre <devbylanre@gmail.com>
2024-08-06 22:25:11 +0100

    type(user): new type - Action, State, Context

--- src/types/user.ts
+++ src/types/user.ts
@@ -1,11 +1,27 @@
+import React, { SetStateAction } from 'react';
+
 namespace User {
-  export type Props = {
+  export type Type = {
     _id: string;
     firstName: string;
     lastName: string;
     role: string;
     email: string;
     status: string;
+  };
+
+  export type Action =
+    | { type: 'GET'; payload: Type }
+    | { type: 'UPDATE'; payload: Type }
+    | { type: 'DELETE'; payload: undefined };
+
+  export type State = {
+    user: Type | undefined;
+  };
+
+  export type Context = {
+    state: State;
+    dispatch: React.Dispatch<Action>;
   };
 }
 
