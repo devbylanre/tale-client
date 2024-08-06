d13231561aa022e0f1061627c7b79ddb8d7235ae
devbylanre <devbylanre@gmail.com>
2024-08-06 23:19:52 +0100

    feat(media detail): removed query logic

--- src/features/mediaDetail/Feature.tsx
+++ src/features/mediaDetail/Feature.tsx
@@ -1,34 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Base from './components/Base';
 import Information from './components/Information';
 import Actions from './components/Actions';
-import { useLazyQuery } from '@apollo/client';
-import { GET_SINGLE_MEDIA } from '../../apis/mediaApi';
-import { useSearchParams } from 'react-router-dom';
-import Media from '../../types/media';
-import useMedia from '../../hooks/useMedia';
 
 const MediaDetailFeature = () => {
-  const { setMedia } = useMedia();
-  const [searchParams] = useSearchParams();
-  const ID = searchParams.get('mediaId');
-
-  const [getMedia] = useLazyQuery<{
-    media: Media.Type;
-  }>(GET_SINGLE_MEDIA, {
-    variables: { id: ID },
-    onCompleted: ({ media }) => {
-      setMedia({ type: 'READ_SINGLE', payload: media });
-    },
-  });
-
-  useEffect(() => {
-    if (ID === null) return;
-    getMedia();
-
-    return () => {};
-  }, [ID, getMedia]);
-
   return (
     <React.Fragment>
       <Base />
