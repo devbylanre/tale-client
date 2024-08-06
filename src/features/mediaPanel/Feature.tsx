396a96e2c68571af70aa4223fc5543be0d8b50c8
devbylanre <devbylanre@gmail.com>
2024-08-06 23:25:16 +0100

    feat(media panel): fetch media data and update state

--- src/features/mediaPanel/Feature.tsx
+++ src/features/mediaPanel/Feature.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './components/Header';
+import { useSearchParams } from 'react-router-dom';
+import useMedia from '../../hooks/useMedia';
+import { useLazyQuery } from '@apollo/client';
+import Media from '../../types/media';
+import { GET_SINGLE_MEDIA } from '../../apis/mediaApi';
 
 const MediaPanelFeature = () => {
+  const { setMedia } = useMedia();
+  const [searchParams] = useSearchParams();
+  const mediaID = searchParams.get('mediaId');
+
+  const [getMedia] = useLazyQuery<{
+    media: Media.Type;
+  }>(GET_SINGLE_MEDIA, {
+    variables: { id: mediaID },
+    onCompleted: ({ media }) => {
+      setMedia({ type: 'READ_SINGLE', payload: media });
+    },
+  });
+
+  useEffect(() => {
+    if (mediaID === null) return;
+    getMedia();
+  }, [mediaID, getMedia]);
+
   return (
     <React.Fragment>
       <Header />
