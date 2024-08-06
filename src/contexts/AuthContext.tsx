9f9262b276a6f439a44c63647374b71fcc62ef23
devbylanre <devbylanre@gmail.com>
2024-08-06 22:37:02 +0100

    context(auth): reducer and provider component setup

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useEffect, useReducer } from 'react';
+import Auth from '../types/auth';
+
+const initialState: Auth.State = { isLoggedIn: false };
+
+export const AuthContext = createContext<Auth.Context | null>(null);
+
+const reducer = (_: Auth.State, action: Auth.Action): Auth.State => {
+  switch (action.type) {
+    case 'LOGGED_IN':
+      return { isLoggedIn: true };
+    case 'LOGGED_OUT':
+      return { isLoggedIn: false };
+  }
+};
+
+const AuthProvider = ({ children }: React.PropsWithChildren) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    let accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      dispatch({ type: 'LOGGED_OUT', payload: { isLoggedIn: false } });
+    }
+
+    return () => {
+      dispatch({ type: 'LOGGED_IN', payload: { isLoggedIn: true } });
+    };
+  });
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
