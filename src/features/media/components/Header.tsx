c075ebb60a557584519da8ab6b9e78b4563266af
devbylanre <devbylanre@gmail.com>
2024-08-06 23:05:18 +0100

    feat(media): revamped component

--- src/features/media/components/Header.tsx
+++ src/features/media/components/Header.tsx
@@ -7,20 +7,16 @@ const Header = () => {
   return (
     <Flex
       px={'lg'}
+      height={'48'}
       borderBottom={'1'}
       position={'sticky'}
       alignItems={'center'}
       borderColor={'gray-95'}
       justifyContent={'between'}
       backgroundColor={'white'}
-      style={{ top: '0rem', zIndex: 1, height: 'var(--header-panel)' }}
+      style={{ top: '0rem', zIndex: 1 }}
     >
-      <Text
-        as={'p'}
-        color={'gray-40'}
-      >
-        Manage your docs, videos, audios and more...
-      </Text>
+      <Text as={'p'}>Manage your docs, videos, audios and more...</Text>
 
       <MediaUploadFeature />
     </Flex>
